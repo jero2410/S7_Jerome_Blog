c37b829d12d27f967c15ce686de525914a9f62d9
Jerome <jeromenabil9@gmail.com>
2025-09-23 00:33:38 +0300

    intial commit

--- Blog.APIs/obj/Debug/net8.0/Blog.APIs.AssemblyInfo.cs
+++ Blog.APIs/obj/Debug/net8.0/Blog.APIs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.APIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a303020d5cf829c5e57537ddb1f12fb04252376")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.APIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.APIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
